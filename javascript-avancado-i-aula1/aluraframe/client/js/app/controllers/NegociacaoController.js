e87232ea1b8a9279f818279456b55c385978a000
Unknown <cyborgmg@gmail.com>
2018-02-12 13:30:58 -0200

    12-fev-2018

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,26 +6,34 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade =  $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+
+        this._inputData.focus();
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
+              DateHelper.textoParaData(this._inputData.value),
+              this._inputQuantidade.value,
+              this._inputValor.value
+            );
+    }        
 
     adiciona(event) {
 
         event.preventDefault();
 
-        let data = DateHelper.textoParaData(this._inputData.value);
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
 
-        let negociacao = new Negociacao(
-                data,
-                this._inputQuantidade.value,
-                this._inputValor.value
-            );
-
-
-        console.log(negociacao);
-
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+        console.log(this._listaNegociacoes.negociacoes);
     }
-
-
 
   }
