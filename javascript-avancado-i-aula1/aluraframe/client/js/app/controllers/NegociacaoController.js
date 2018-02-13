5f2fb5914375e88550e197b9b69fb0b876b092d6
Unknown <cyborgmg@gmail.com>
2018-02-13 13:44:15 -0200

    13-fev-2018

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -9,13 +9,14 @@ class NegociacaoController {
         this._inputQuantidade =  $('#quantidade');
         this._inputValor = $('#valor');
 
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model) );
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
+        
 
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        //this._mensagemView.update(this._mensagem);
     }
 
     _limpaFormulario() {
@@ -40,7 +41,7 @@ class NegociacaoController {
         event.preventDefault();
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
+        //this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = 'Negociacao adicionada com sucesso';
         this._mensagemView.update(this._mensagem);  
@@ -51,7 +52,7 @@ class NegociacaoController {
     apaga() {
 
         this._listaNegociacoes.esvazia();
-        this._negociacoesView.update(this._listaNegociacoes);
+        //this._negociacoesView.update(this._listaNegociacoes);
     
         this._mensagem.texto = 'Negociações apagadas com sucesso';
         this._mensagemView.update(this._mensagem);
